738fd0dcef5b64eea38c01dcda9b2cdcc4c9b3a6
Isaac Batista <isaacbatst@gmail.com>
2022-07-13 11:15:30 -0300

    add celebration messages

--- src/useCases/botEvents/incrementArthurFowards.ts
+++ src/useCases/botEvents/incrementArthurFowards.ts
@@ -2,6 +2,11 @@ import TelegramBot from "node-telegram-bot-api";
 import { PersonModel } from "../../database/Entities/Person/Person";
 import drawName from "../../names";
 
+const celebrationNumbers: number[] = [500, 1000, 2000];
+
+const differenceTenMessage = (number: number) => `A essa altura espero que o role dos ${number} já tenha data, local e atrações confirmadas`
+const differenceTenPercentMessage = (number: number) => `Quase nos ${number}, podem começar a marcar o churrasco, vai ser na casa de quem?`
+
 export const incrementArthurFowards = async (message: TelegramBot.Message, bot: TelegramBot) => {
   const name = 'Arthur';
 
@@ -11,5 +16,27 @@ export const incrementArthurFowards = async (message: TelegramBot.Message, bot: 
   const nicknames = await PersonModel.getNicknames(name);
   const nickname = drawName(nicknames);
 
-  return bot.sendMessage(message.chat.id, `${nickname} dividiu e compartilhou ${counter} vezes`)
+  const celebrationMessage = celebrationNumbers.reduce<string | null>((acc, number) => {
+    if(acc) return acc;
+
+    const isDifferenceTenPercent = (number - counter) === (number * 0.1);
+    
+    if(isDifferenceTenPercent){
+      return differenceTenPercentMessage(number)
+    }
+
+    const isDifferenceTen = (number - counter) === 10;
+
+    if(isDifferenceTen) {
+      return differenceTenMessage(number)
+    }
+
+    return null;
+  }, null)
+
+  bot.sendMessage(message.chat.id, `${nickname} dividiu e compartilhou ${counter} vezes`)
+
+  if(celebrationMessage) {
+    bot.sendMessage(message.chat.id, celebrationMessage)
+  }
 }
