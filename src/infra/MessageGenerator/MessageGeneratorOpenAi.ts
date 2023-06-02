6cf25c5b0a9e3177601637e67f1cee9c28a36d4c
Isaac Batista <isaacbatst@gmail.com>
2023-06-02 14:16:52 -0300

    improve ai setup

--- src/infra/MessageGenerator/MessageGeneratorOpenAi.ts
+++ src/infra/MessageGenerator/MessageGeneratorOpenAi.ts
@@ -20,10 +20,17 @@ export class MessageGeneratorOpenAi implements MessageGenerator {
     
     this.openAiApi = new OpenAIApi(configuration)
     this.arthurActionMessage = 'Um amigo acabou de compartilhar um meme, reaja a ele.'
-    this.luanSetupMessage = "Você é um bot que reage quando um amigo compartilha um canal de uma streamer. Sua reação é uma mensagem curta "
-+ ", com no máximo 15 palavras. A mensagem não terá letras maiúsculas. A mensagem tem 20% de chance de ser sarcástica."
-+ "A linguagem deve ser coloquial. A mensagem terá será como mensagens de jovens em redes sociais, sem letras maiúsculas,"
-+ "com emojis, sem acentos ortográficos e escrita com abreviações."
+    this.luanSetupMessage = "Você é um bot imitando um jovem conversando em redes sociais. "
++ "Você responde quando um amigo compartilha um canal de uma streamer e faz uma piada sarcástica com o conteúdo dela. " 
++ "Suas respostas tem TODAS características a seguir:" 
++ " - No máximo 15 palavras. \n"
++ " - Tem todas as letras minúsculas. \n"
++ " - Usa emojis e palavras abreviadas, como os jovens conversam em redes sociais. \n"
++ " - Usa gírias como: 'fmz', 'slc', 'top', 'nice'. \n"
++ " - Não usa hashtags. \n"
++ " - Não usa # \n"
++ " - Chama amigo pelo nome dele."
++ " - Tem uma referência ao conteúdo da stream que foi compartilhada."
   }
 
   private makeArthurSetupMessage(name: string): string {
@@ -35,18 +42,17 @@ Chame o autor do meme de ${name.toLowerCase()}`
   }
 
   private makeLuanActionMessage(name: string, channelUrl: string): string {
-    return `Um amigo acaba compartilhar esse canal ${channelUrl} sua mensagem deve fazer referência ao tipo de conteúdo dele. \
-Chame o autor do post de ${name.toLowerCase()}.`
+    return `${name.toLowerCase()}: ${channelUrl}`
   }
 
   async generateLuanAmouranthMessage(nickname: string, channelUrl: string): Promise<string> {
     try {
       const {data} = await this.openAiApi.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        temperature: 1,
+        temperature: 1.2,
         messages: [
           {role: 'system', content: this.luanSetupMessage },
-          {role: 'system', content: this.makeLuanActionMessage(nickname, channelUrl) }
+          {role: 'user', content: this.makeLuanActionMessage(nickname, channelUrl) }
         ],
         max_tokens: 50,
       })
@@ -59,7 +65,7 @@ Chame o autor do post de ${name.toLowerCase()}.`
   
       return firstChoice.message.content
     } catch (err) {
-      return this.fallbackGenerator.generateArthurMessage()
+      return this.fallbackGenerator.generateLuanAmouranthMessage()
     }
   }
 
